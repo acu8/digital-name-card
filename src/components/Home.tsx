75125af3ecb5371c877186d233067bd552c00a00
acu8 <ewatanabe786@gmail.com>
2024-08-03 15:01:37 +0900

    UI

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,22 +1,96 @@
-import { Box, Heading, Link as ChakraLink } from "@chakra-ui/react";
+import {
+  Box,
+  Heading,
+  Link as ChakraLink,
+  Flex,
+  Container,
+  VStack,
+  Card,
+  CardBody,
+} from "@chakra-ui/react";
+/** @jsxImportSource @emotion/react */
+import { css, keyframes } from "@emotion/react";
 import { Link } from "react-router-dom";
+
+const bounce = keyframes`
+  0%, 100% { transform: translateY(0); }
+  50% { transform: translateY(-10px); }
+`;
+
+const bounceStyle = css`
+  &:hover {
+    animation: ${bounce} 0.3s ease infinite;
+  }
+`;
 
 function Home() {
   return (
-    <Box p={5}>
-      <Heading mb={4}>Welcome to Digital Name Card</Heading>
-      <ChakraLink as={Link} to="/cards/sample-id">
-        View Example Card
-      </ChakraLink>
-      <br />
-      <ChakraLink as={Link} to="/cards/register">
-        Go to Register Page
-      </ChakraLink>
-      <br />
-      <ChakraLink as={Link} to="/cards/search">
-        Go to Search Page
-      </ChakraLink>
-    </Box>
+    <Flex minHeight="100vh" alignItems="center" justifyContent="center">
+      <Container maxW="md" centerContent>
+        <VStack spacing={6} align="stretch">
+          <Heading mb={2} textAlign="center">
+            Digital Name Card
+          </Heading>
+          <ChakraLink
+            as={Link}
+            to="/cards/sample-id"
+            _hover={{ textDecoration: "none" }}
+            css={bounceStyle}
+          >
+            <Card
+              boxShadow="md"
+              backgroundColor="#FFFDE6"
+              borderRadius="2xl"
+              _hover={{ backgroundColor: "#E6FFF0" }}
+              cursor="pointer"
+              p={3}
+            >
+              <CardBody py={2} textAlign="center">
+                View Example Card
+              </CardBody>
+            </Card>
+          </ChakraLink>
+          <ChakraLink
+            as={Link}
+            to="/cards/register"
+            _hover={{ textDecoration: "none" }}
+            css={bounceStyle}
+          >
+            <Card
+              boxShadow="md"
+              backgroundColor="#FFFDE6"
+              borderRadius="2xl"
+              _hover={{ backgroundColor: "#E6FFF0" }}
+              cursor="pointer"
+              p={3}
+            >
+              <CardBody py={2} textAlign="center">
+                Go to Register Page
+              </CardBody>
+            </Card>
+          </ChakraLink>
+          <ChakraLink
+            as={Link}
+            to="/cards/search"
+            _hover={{ textDecoration: "none" }}
+            css={bounceStyle}
+          >
+            <Card
+              boxShadow="md"
+              backgroundColor="#FFFDE6"
+              borderRadius="2xl"
+              _hover={{ backgroundColor: "#E6FFF0" }}
+              cursor="pointer"
+              p={3}
+            >
+              <CardBody py={2} textAlign="center">
+                Go to Search Page
+              </CardBody>
+            </Card>
+          </ChakraLink>
+        </VStack>
+      </Container>
+    </Flex>
   );
 }
 
